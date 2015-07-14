05213dade36b500f77ade95760eefb880e52bcf2
Prasanthi Relangi <prasanthi@Prasanthis-MacBook-Pro.local>
2015-07-13 21:35:24 -0700

    Initial Commit

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Prasanthi Relangi on 7/13/15.
+//  Copyright (c) 2015 prasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
